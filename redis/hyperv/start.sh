c92fe968ad99b8e63a724ecf9c40e7d76e5fccdc
lzxm160 <lzxm160@sina.com>
2017-03-30 16:52:17 +0800

    hyperv

---
+++ redis/hyperv/start.sh
@@ -0,0 +1,7 @@
+su redis
+redis-server /etc/redis/redis-6380.conf
+redis-server /etc/redis/redis-6381.conf
+redis-server /etc/redis/redis-7382.conf
+redis-server /etc/redis/redis-6382.conf
+redis-server /etc/redis/redis-7380.conf
+redis-server /etc/redis/redis-7381.conf
