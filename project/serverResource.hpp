e19bf82aad418485bacae333118c439bd81541ad
lzxm160 <lzxm160@sina.com>
2017-05-22 21:24:05 +0800

    token

--- project/serverResource.hpp
+++ project/serverResource.hpp
@@ -4,7 +4,7 @@
 #include "serverResource_base.hpp"
 // #include "json_parser/json_map.hpp"
 // #include "json_parser/json_fifo_map.hpp"
-#include "json_parser/json.hpp"
+// #include "json_parser/json.hpp"
 void deal_with_flow_number(HttpServer::Response& response, std::shared_ptr<HttpServer::Request> request)
 {
      try 
